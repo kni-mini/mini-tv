317e51f62493244c87fe14e09bea03f744362729
Mateusz Polis <polismateusz@gmail.com>
2025-04-17 12:30:34 +0200

    feat: add startDate and endDate fields to announcements and posters schemas

--- db/schema/announcements.ts
+++ db/schema/announcements.ts
@@ -3,14 +3,20 @@ import { users } from './users';
 import { groups } from './groups';
 import { uniqueWhenNotDeleted } from './utils';
 
-export const announcements = pgTable('announcements', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  message: text('message').notNull(), // markdown
-  groupId: integer('group_id').references(() => groups.id),
-  userId: integer('user_id').notNull().references(() => users.id),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  deletedAt: timestamp('deleted_at'),
-}, (table) => [
-  uniqueWhenNotDeleted(table, 'name')(index()),
-]); 
+export const announcements = pgTable(
+  'announcements',
+  {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    message: text('message').notNull(), // markdown
+    groupId: integer('group_id').references(() => groups.id),
+    userId: integer('user_id')
+      .notNull()
+      .references(() => users.id),
+    startDate: timestamp('start_date').defaultNow().notNull(),
+    endDate: timestamp('end_date'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    deletedAt: timestamp('deleted_at'),
+  },
+  (table) => [uniqueWhenNotDeleted(table, 'name')(index())]
+);
--- db/schema/posters.ts
+++ db/schema/posters.ts
@@ -4,14 +4,22 @@ import { groups } from './groups';
 import { images } from './images';
 import { uniqueWhenNotDeleted } from './utils';
 
-export const posters = pgTable('posters', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  imageId: integer('image_id').notNull().references(() => images.id),
-  groupId: integer('group_id').references(() => groups.id),
-  userId: integer('user_id').notNull().references(() => users.id),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  deletedAt: timestamp('deleted_at'),
-}, (table) => [
-  uniqueWhenNotDeleted(table, 'name')(index()),
-]); 
+export const posters = pgTable(
+  'posters',
+  {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    imageId: integer('image_id')
+      .notNull()
+      .references(() => images.id),
+    groupId: integer('group_id').references(() => groups.id),
+    userId: integer('user_id')
+      .notNull()
+      .references(() => users.id),
+    startDate: timestamp('start_date').defaultNow().notNull(),
+    endDate: timestamp('end_date'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    deletedAt: timestamp('deleted_at'),
+  },
+  (table) => [uniqueWhenNotDeleted(table, 'name')(index())]
+);
