87be82729264308c9927cdc4ae652308b0abc8f3
Mateusz Polis <polismateusz@gmail.com>
2025-04-18 10:14:21 +0200

    feat: add group_types and user_organizations tables, update groups and users schemas to reference new IDs

--- db/migrations/0000_initial_database_setup.sql
+++ db/migrations/0000_initial_database_setup.sql
@@ -1,5 +1,3 @@
-CREATE TYPE "public"."group_type" AS ENUM('event', 'announcement', 'poster');--> statement-breakpoint
-CREATE TYPE "public"."user_role" AS ENUM('dean', 'student_council', 'student_club');--> statement-breakpoint
 CREATE TABLE "announcements" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
@@ -24,11 +22,19 @@ CREATE TABLE "events" (
 	"deleted_at" timestamp
 );
 --> statement-breakpoint
+CREATE TABLE "group_types" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp,
+	CONSTRAINT "group_types_name_unique" UNIQUE("name")
+);
+--> statement-breakpoint
 CREATE TABLE "groups" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
 	"color" text NOT NULL,
-	"type" "group_type" NOT NULL,
+	"type_id" integer NOT NULL,
 	"user_id" integer NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_at" timestamp
@@ -38,6 +44,15 @@ CREATE TABLE "images" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"file" text NOT NULL,
 	"name" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "organizations" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"short_name" text NOT NULL,
+	"image_id" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_at" timestamp
 );
@@ -54,21 +69,18 @@ CREATE TABLE "posters" (
 	"deleted_at" timestamp
 );
 --> statement-breakpoint
-CREATE TABLE "student_clubs" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"short_name" text NOT NULL,
-	"image_id" text NOT NULL,
+CREATE TABLE "user_organizations" (
+	"user_id" integer NOT NULL,
+	"organization_id" integer NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"deleted_at" timestamp
+	"deleted_at" timestamp,
+	CONSTRAINT "user_organizations_user_id_organization_id_pk" PRIMARY KEY("user_id","organization_id")
 );
 --> statement-breakpoint
 CREATE TABLE "users" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"username" text NOT NULL,
 	"password" text NOT NULL,
-	"role" "user_role" NOT NULL,
-	"student_club_id" integer,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_at" timestamp
 );
@@ -77,15 +89,17 @@ ALTER TABLE "announcements" ADD CONSTRAINT "announcements_group_id_groups_id_fk"
 ALTER TABLE "announcements" ADD CONSTRAINT "announcements_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "events" ADD CONSTRAINT "events_group_id_groups_id_fk" FOREIGN KEY ("group_id") REFERENCES "public"."groups"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "events" ADD CONSTRAINT "events_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "groups" ADD CONSTRAINT "groups_type_id_group_types_id_fk" FOREIGN KEY ("type_id") REFERENCES "public"."group_types"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "groups" ADD CONSTRAINT "groups_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "posters" ADD CONSTRAINT "posters_image_id_images_id_fk" FOREIGN KEY ("image_id") REFERENCES "public"."images"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "posters" ADD CONSTRAINT "posters_group_id_groups_id_fk" FOREIGN KEY ("group_id") REFERENCES "public"."groups"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "posters" ADD CONSTRAINT "posters_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_student_club_id_student_clubs_id_fk" FOREIGN KEY ("student_club_id") REFERENCES "public"."student_clubs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_organizations" ADD CONSTRAINT "user_organizations_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_organizations" ADD CONSTRAINT "user_organizations_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "announcements_name_index" ON "announcements" USING btree ("name") WHERE "announcements"."deleted_at" IS NULL;--> statement-breakpoint
 CREATE INDEX "events_name_index" ON "events" USING btree ("name") WHERE "events"."deleted_at" IS NULL;--> statement-breakpoint
 CREATE INDEX "groups_name_index" ON "groups" USING btree ("name") WHERE "groups"."deleted_at" IS NULL;--> statement-breakpoint
+CREATE INDEX "organizations_name_index" ON "organizations" USING btree ("name") WHERE "organizations"."deleted_at" IS NULL;--> statement-breakpoint
+CREATE INDEX "organizations_short_name_index" ON "organizations" USING btree ("short_name") WHERE "organizations"."deleted_at" IS NULL;--> statement-breakpoint
 CREATE INDEX "posters_name_index" ON "posters" USING btree ("name") WHERE "posters"."deleted_at" IS NULL;--> statement-breakpoint
-CREATE INDEX "student_clubs_name_index" ON "student_clubs" USING btree ("name") WHERE "student_clubs"."deleted_at" IS NULL;--> statement-breakpoint
-CREATE INDEX "student_clubs_short_name_index" ON "student_clubs" USING btree ("short_name") WHERE "student_clubs"."deleted_at" IS NULL;--> statement-breakpoint
 CREATE INDEX "users_username_index" ON "users" USING btree ("username") WHERE "users"."deleted_at" IS NULL;
