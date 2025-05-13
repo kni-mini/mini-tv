c4571dceb46fd9763977f259a61b66319f84eccf
Matios102 <matios102@gmail.com>
2025-05-13 19:26:28 +0200

    feat: rename images table to medias and update related migrations

--- db/schema/medias.ts
+++ db/schema/medias.ts
@@ -2,7 +2,7 @@ import { pgTable, serial, text, timestamp, pgEnum } from 'drizzle-orm/pg-core';
 
 export const mediaTypeEnum = pgEnum('media_type', ['image', 'gif', 'video']);
 
-export const medias = pgTable('images', {
+export const medias = pgTable('medias', {
   id: serial('id').primaryKey(),
   file: text('file').notNull(), // base64 encoded file
   name: text('name'),
