7487a8e62e78862bbda0ffb17be083df010b5e1c
Ahsodecas <nastya22072005k@gmail.com>
2025-05-20 21:54:45 +0200

    The created announcement is saved to the database (partially for now)

--- src/app/admin/dashboard/student-club-announcement/actions.ts
+++ src/app/admin/dashboard/student-club-announcement/actions.ts
@@ -6,8 +6,13 @@ import { z } from "zod";
 import ClubAnnouncement from "@/Components/ClubAnnouncement"
 import type {AnnouncementCardProps} from "@/Components/ClubAnnouncement"
 
-let sql = postgres(process.env.DATABASE_URL || process.env.POSTGRES_URL!, {
-  ssl: "allow",
+const sql = postgres({
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.POSTGRES_PORT || 5432),
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || '',
+  database: process.env.POSTGRES_DB || 'postgres',
+  ssl: false,
 });
 
 const clubAnnouncementSchema = z.object({
@@ -28,30 +33,43 @@ export async function createClubAnnouncement(formData: FormData) {
     let media_url = null;
     let icon_url = null;
 
-    if (!parse.success) {
+    if (!parse.success) 
+    {
         throw new Error("Invalid form submission");
     }
 
-   /*
-   // Add new announcement to database
-   try {
+     let mediaId: number | null = null;
+
+    try {
+        if (media && media.size > 0) 
+        {
+            const arrayBuffer = await media.arrayBuffer();
+            const base64Media = Buffer.from(arrayBuffer).toString("base64");
+            const mediaType = "image"; // todo
+            // todo media name, created at, deleted at
+
+            const [insertedMedia] = await sql`
+                INSERT INTO medias (file, name, type)
+                VALUES (${base64Media}, ${media.name}, ${mediaType})
+                RETURNING id
+            `;
+
+            mediaId = insertedMedia.id;
+        }
+        const organizationId = 1; // todo
+        const groupId = null;     // todo
+
         await sql`
-        INSERT INTO announcements (name, message, media_url, icon_url)
-        VALUES (${name}, ${message}, ${media_url}, ${icon_url})
-        `;
+            INSERT INTO announcements (name, message, media_id, organization_id, group_id)
+            VALUES (${name}, ${message}, ${mediaId}, ${organizationId}, ${groupId})
+            `;
 
         revalidatePath("/admin/dashboard/student-club-announcement");
         //return { message: `Added todo ${data.todo}` };
-    } catch (e) {
+    }
+    catch (e: any) 
+    {
+        console.error("DB Error:", e);
         throw new Error("Failed to create a student club announcement");
-    }*/
-    const newAnnouncementProps : AnnouncementCardProps = {
-        title:name, 
-        body:message, 
-        clubName:name, 
-        mediaSrc:undefined, 
-        logoSrc:undefined, 
-        mediaType:undefined};
-
-    const newAnnouncement = ClubAnnouncement ({...newAnnouncementProps});
+    }
   }
